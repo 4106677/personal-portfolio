278210a33c8808149cc50fbd4d363a63001acfac
Yaroslav Oliinyk <4106677@gmail.com>
2023-05-11 20:48:21 +0300

    homePage finish

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="http://www.linkedin.com/in/JOn4106677"
+        className="home__social-icon"
+        target="_blanc"
+      >
+        <i className="uil uil-linkedin-alt"></i>
+      </a>
+      <a
+        href="https://github.com/4106677"
+        className="home__social-icon"
+        target="_blanc"
+      >
+        <i className="uil uil-github-alt"></i>
+      </a>
+      <a
+        href="https://t.me/YO4106677"
+        className="home__social-icon"
+        target="_blanc"
+      >
+        <i className="uil uil-telegram-alt"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
