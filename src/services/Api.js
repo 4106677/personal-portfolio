7d03824ea3d2cd2202d9d7d23cfe256ee195e6e8
Yaroslav <4106677@gmail.com>
2026-08-11 16:01:04 +0300

    add reviews

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,5 +1,12 @@
 import axios from "axios";
 import { projectsData } from "./buckupData";
+import Vitalii_Kablukov from '../assets/photos/Vitalii_Kablukov.jpeg'
+import Kateryna_Ostrynska from '../assets/photos/Kateryna_Ostrynska.jpeg'
+import Anna_Zauholnikova from '../assets/photos/Anna_Zauholnikova.png'
+import Oleksandr_Pavlov from '../assets/photos/Oleksandr_Pavlov.png'
+import Den_Belo from '../assets/photos/Den_Belo.jpeg'
+import Tanya_Yatsukhnenko from '../assets/photos/Tanya_Yatsukhnenko.jpeg'
+import Tymur_Arabadzhy from '../assets/photos/Tymur_Arabadzhy.jpeg'
 
 axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
 
@@ -33,36 +40,48 @@ const skillsList = [
 
 export const reviews = [
   {
-    id: 1,
-    image:
-      "https://media.licdn.com/dms/image/D4D35AQEIRcKEQ0jgLA/profile-framedphoto-shrink_800_800/0/1671059863943?e=1696777200&v=beta&t=bfroBA1sXjqe-Cdxorv-68Wx66-ig26pDTm-dpmQhNc",
-    title: "Vitalii Kablukov",
-    description:
-      "In the team, Yaroslav showed very cool skills as a Team-leader and incredible work in the team. this man have a good potential.",
-  },
-  {
     id: 2,
-    image:
-      "https://media.licdn.com/dms/image/D4D03AQGvtEKoagqYiw/profile-displayphoto-shrink_200_200/0/1693054100201?e=1701907200&v=beta&t=tFbn1JcyEJF3HOzjHM0UD5ALCILhm6dMzmJ_1xuKCb8",
-    title: "Kateryna Kononenko",
-    description:
-      "With Yaroslav we have worked together on a project, and I found him a highly skilled and dedicated professional. As team leader he was very responsible and orginized. His expertise in the field of IT developing and his leadership has helped our team to get a great result. I highly recommend him to anyone looking for help on their team or project.",
-  },
-  {
-    id: 3,
-    image:
-      "https://media.licdn.com/dms/image/D4D03AQGXA2-dWKh9gw/profile-displayphoto-shrink_200_200/0/1673722147830?e=1701907200&v=beta&t=vcqqHo3-b9x81nRMhn5imqiI1L3dccqvBMdtfx7zkMk",
-    title: "Anna Zauholnikova",
-    description:
-      "Yaroslav Oliinyk works well in a team, communicates easily with colleagues. Also during the Fullstack Developer course he showed himself in the best way as a team leader on team projects. Actively helped the team with solving any problem in the application code or technical problems on the project. Yaroslav is responsible, attentive to details and proactive.",
-  },
-  {
-    id: 4,
-    image:
-      "https://media.licdn.com/dms/image/C4D03AQGMIwJv00NR2g/profile-displayphoto-shrink_200_200/0/1651482629087?e=1701907200&v=beta&t=JMjTEzc4uARNJo0JFsHHxgnE0M89OdSyDtDrlnI_-3c",
+    image: Oleksandr_Pavlov,
     title: "Oleksandr Pavlov",
     description:
       "We worked with Yaroslav on few team projects during studying at GoIT. Yaroslav is a strong team player, responsible person and effective developer. You can always be sure that if some task was given to him — it will be done. Only positive impressions from cooperation. Good luck!",
+  }, {
+    id: 3,
+    image: Tanya_Yatsukhnenko,
+    title: "Tanya Yatsukhnenko",
+    description:
+      "Yaroslav is a highly skilled JavaScript developer with extensive experience in building robust web applications. Their proficiency in JavaScript and related frameworks has been instrumental in the success of multiple projects. Yaroslav consistently delivers high-quality code, meets deadlines, and effectively collaborates with team members. Their problem-solving abilities and attention to detail make them a valuable asset to any development team. I highly recommend Yaroslav for any project requiring exceptional JavaScript development skills."  }, {
+    id: 4,
+    image: Tymur_Arabadzhy,
+    title: "Tymur Arabadzhy",
+    description:
+      "Профессионал!!!" },
+  {
+    id: 5,
+    image: Den_Belo,
+    title: "Денис Белостоцкий",
+    description:
+      "Yaroslav is an exceptional JavaScript developer with extensive experience in building high-quality web applications. They deliver clean, efficient code and consistently meet deadlines. Their strong problem-solving skills and ability to collaborate effectively with team members make them a valuable asset to any project. Highly recommended for their expertise and professionalism."  },
+  {
+    id: 5,
+    image: Vitalii_Kablukov,
+    title: "Vitalii Kablukov",
+    description:
+        "In the team, Yaroslav showed very cool skills as a Team-leader and incredible work in the team. this man have a good potential.",
+  },
+  {
+    id: 6,
+    image: Kateryna_Ostrynska,
+    title: "Kateryna Ostrynska",
+    description:
+        "With Yaroslav we have worked together on a project, and I found him a highly skilled and dedicated professional. As team leader he was very responsible and orginized. His expertise in the field of IT developing and his leadership has helped our team to get a great result. I highly recommend him to anyone looking for help on their team or project.",
+  },
+  {
+    id: 7,
+    image: Anna_Zauholnikova,
+    title: "Anna Zauholnikova",
+    description:
+        "Yaroslav Oliinyk works well in a team, communicates easily with colleagues. Also during the Fullstack Developer course he showed himself in the best way as a team leader on team projects. Actively helped the team with solving any problem in the application code or technical problems on the project. Yaroslav is responsible, attentive to details and proactive.",
   },
 ];
 
